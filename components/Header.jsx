4a9d228230d7af46fb35fd6cfb12f2fdda329534
keplar-404 <shehabhossen76.services@gmail.com>
2023-04-10 00:29:15 +0600

    complited navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import Line from "../public/m.svg"
+import Line from "../public/m.svg";
 
-function Header() {
-  const NavBarText = ({ data, setToggleMenu }) => {
-    return (
-      <>
-        <p
-          className="mt-8 w-fit cursor-pointer hover:text-gray-400"
-          onClick={data === "Close" ? () => setToggleMenu(false) : () => ""}
-        >
-          <Link href={data === "Close" ? `` : `/${data}`}>{data}</Link>
-        </p>
-      </>
-    );
+const NavBarText = ({ data, setToggleMenu }) => {
+  const handleClick = () => {
+    if (data === "Close") {
+      setToggleMenu(false);
+    }
   };
 
+  return (
+    <p
+      className="sm:mt-8 md:mt-8 lg:mt-0 xl:mt-0 2xl:mt-0 w-fit cursor-pointer navText"
+      onClick={handleClick}
+    >
+      <Link href={data === "Close" ? `` : `/${data}`}>{data}</Link>
+    </p>
+  );
+};
+
+const Header = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
-  const navBarItems = [
+  const navBarItemsForMobile = [
     "Features",
     "Certificate",
     "Company",
@@ -25,57 +29,76 @@ function Header() {
     "Download",
     "Close",
   ];
+  const navBarItemsForAllDevices = [
+    "Features",
+    "Certificate",
+    "Company",
+    "Lunch app",
+    "Download",
+  ];
 
   return (
     <>
-
       {/* mobile menu */}
       <div
-        className={`${toggleMenu === false ? "sm:hidden" : "sm:visible"}
-        absolute flex flex-col items-center pt-12 w-screen h-screen  nav-bg text-white font-bold global-titleFont  text-sm`}
+        className={`lg:hidden xl:hidden 2xl:hidden ${
+          toggleMenu ? "sm:visible md:visible" : "sm:hidden md:hidden"
+        } absolute flex flex-col items-center pt-12 w-screen h-screen nav-bg text-white font-bold global-titleFont text-sm`}
       >
-        {navBarItems.map((data) => (
-          <NavBarText data={data} setToggleMenu={setToggleMenu} />
+        {navBarItemsForMobile.map((data) => (
+          <NavBarText
+            key={data}
+            data={data}
+            setToggleMenu={setToggleMenu}
+          />
         ))}
       </div>
 
-    <div className="text-white font-bold global-titleFont bg-global text-sm pt-[3vh]">
-
-
-{/* all devices menu */}
-      <div className=" grid grid-cols-2">
-        {/* logo left */}
-        <div className="flex items-center">
-          <Link href={"/"}>
-            <div className="flex cursor-pointer">
-              <img
-                src="./logo.png"
-                alt="logo"
-                className="w-auto sm:h-4 my-auto"
-              />
-              <p>SkillSpring</p>
+      {/* menu for other devices */}
+      <div className="text-white font-bold global-titleFont bg-global text-sm pt-[3vh] px-[8vw]">
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3">
+          {/* left */}
+          <div className="flex items-center gap-x-[2vw] sm:col-span-0 md:col-span-0 lg:col-span-2 xl:col-span-2 2xl:col-span-2">
+            <div>
+              <Link href="/">
+                <div className="flex gap-x-2 cursor-pointer">
+                  <img
+                    src="./logo.png"
+                    alt="logo"
+                    className="w-auto h-4 my-auto"
+                  />
+                  <p>SkillSpring</p>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        {/* items right */}
-        <div className=" flex justify-end gap-x-[5vw]">
-          <div className="flex justify-center items-center">
-          <p>Log in</p>
-          </div>
-          <div className="navBtn-bg">
-            <p>Sign up</p>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            {/* mobile hambarger svg */}
-            <button onClick={()=>setToggleMenu(true)}>
 
-            <Line className="w-5"/>
-            </button>
+            <div className="sm:hidden md:hidden lg:flex xl:flex 2xl:flex gap-x-5">
+              {navBarItemsForAllDevices.map((data) => (
+                <NavBarText key={data} data={data} />
+              ))}
+            </div>
+          </div>
+
+          {/* right */}
+          <div className="sm:w-full md:w-full flex justify-end sm:gap-x-[5vw] md:gap-x-[5vw] lg:gap-x-[2vw] xl:gap-x-[2vw] 2xl:gap-x-[2vw]">
+            <div className="flex justify-center items-center">
+              <p className="cursor-pointer">Log in</p>
+            </div>
+            <div className="navBtn-bg cursor-pointer">
+              <button>
+                <p>Sign up</p>
+              </button>
+            </div>
+            <div className="sm:visible md:visible lg:hidden xl:hidden 2xl:hidden flex flex-col justify-center items-center ">
+              {/* mobile hambarger svg */}
+              <button className="" onClick={() => setToggleMenu(true)}>
+                <Line className="w-5" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-                </>
+    </>
   );
 }
 
